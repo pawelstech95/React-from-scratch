700118b59d31852d1fb909729ac348bec002a56a
Paweł Stech <pawelstech95@gmail.com>
2021-02-24 22:03:21 +0100

    start Interactive menu App

--- Start/app.js
+++ Start/app.js
@@ -557,86 +557,99 @@
 // ________________________________________________________________
 // ________________________________________________________________
 // ________________________________________________________________
-const data = {
-  users: [
-    {
-      id: 1,
-      age: 29,
-      name: 'Arek',
-      sex: 'male',
-    },
-    {
-      id: 2,
-      age: 19,
-      name: 'Marek',
-      sex: 'male',
-    },
-    {
-      id: 3,
-      age: 39,
-      name: 'Marta',
-      sex: 'female',
-    },
-  ],
-};
+// const data = {
+//   users: [
+//     {
+//       id: 1,
+//       age: 29,
+//       name: 'Arek',
+//       sex: 'male',
+//     },
+//     {
+//       id: 2,
+//       age: 19,
+//       name: 'Marek',
+//       sex: 'male',
+//     },
+//     {
+//       id: 3,
+//       age: 39,
+//       name: 'Marta',
+//       sex: 'female',
+//     },
+//   ],
+// };
 
-const Item = ({ user }) => (
-  <div className="userInfo">
-    <h1>Użytkownik {user.name}</h1>
-    <p>Informacje o użytkowniku: </p>
-    <p>
-      Wiek Użytkownika: <strong>{user.age}</strong>
-    </p>
-    <p>Płeć użytkownika: {user.sex}</p>
-  </div>
-);
-class ListItems extends React.Component {
-  state = {
-    select: 'all',
-  };
-  handleUsersFilter = (option) => {
-    this.setState({
-      select: option,
-    });
-    console.log(option);
-  };
-  usersList = () => {
-    // debugger
-    let users = this.props.data.users;
-    switch (this.state.select) {
-      case 'all':
-        return users.map((user) => <Item user={user} key={user.id} />);
-      case 'female':
-        users = users.filter((user) => user.sex === 'female');
-        return users.map((user) => <Item user={user} key={user.id} />);
-      case 'male':
-        users = users.filter((user) => user.sex === 'male');
-        return users.map((user) => <Item user={user} key={user.id} />);
-      default:
-        return ' Brak danych';
-    }
-  };
+// const Item = ({ user }) => (
+//   <div className="userInfo">
+//     <h1>Użytkownik {user.name}</h1>
+//     <p>Informacje o użytkowniku: </p>
+//     <p>
+//       Wiek Użytkownika: <strong>{user.age}</strong>
+//     </p>
+//     <p>Płeć użytkownika: {user.sex}</p>
+//   </div>
+// );
+// class ListItems extends React.Component {
+//   state = {
+//     select: 'all',
+//   };
+//   handleUsersFilter = (option) => {
+//     this.setState({
+//       select: option,
+//     });
+//     console.log(option);
+//   };
+//   usersList = () => {
+//     // debugger
+//     let users = this.props.data.users;
+//     switch (this.state.select) {
+//       case 'all':
+//         return users.map((user) => <Item user={user} key={user.id} />);
+//       case 'female':
+//         users = users.filter((user) => user.sex === 'female');
+//         return users.map((user) => <Item user={user} key={user.id} />);
+//       case 'male':
+//         users = users.filter((user) => user.sex === 'male');
+//         return users.map((user) => <Item user={user} key={user.id} />);
+//       default:
+//         return ' Brak danych';
+//     }
+//   };
+//   render() {
+//     // let users = this.props.data.users;
+//     // users = users.filter((user) => user.sex === 'female');
+//     // const Items = users.map((user) => <Item key={user.id} user={user} />);
+//     // return <>{Items}</>;
+
+//     return (
+//       <div>
+//         <button onClick={this.handleUsersFilter.bind(this, 'all')}>
+//           Wszyscy
+//         </button>
+//         <button onClick={this.handleUsersFilter.bind(this, 'female')}>
+//           Kobiety
+//         </button>
+//         <button onClick={this.handleUsersFilter.bind(this, 'male')}>
+//           Mężczyźni
+//         </button>
+//         {this.usersList()}
+//       </div>
+//     );
+//   }
+// }
+
+// ReactDOM.render(<ListItems data={data} />, document.getElementById('root'));
+
+// 43. Projekt 4: interaktywne menu restauracji, cz. 1 - struktura aplikacji
+// ________________________________________________________________
+// ________________________________________________________________
+// ________________________________________________________________
+
+class App extends React.Component {
+  state = {};
   render() {
-    // let users = this.props.data.users;
-    // users = users.filter((user) => user.sex === 'female');
-    // const Items = users.map((user) => <Item key={user.id} user={user} />);
-    // return <>{Items}</>;
-
-    return (
-      <div>
-        <button onClick={this.handleUsersFilter.bind(this, 'all')}>
-          Wszyscy
-        </button>
-        <button onClick={this.handleUsersFilter.bind(this, 'female')}>
-          Kobiety
-        </button>
-        <button onClick={this.handleUsersFilter.bind(this, 'male')}>
-          Mężczyźni
-        </button>
-        {this.usersList()}
-      </div>
-    );
+    return <div>Menu</div>;
   }
 }
 
-ReactDOM.render(<ListItems data={data} />, document.getElementById('root'));

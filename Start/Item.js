b73bce7ccc7001a42fe61c78f2086525c7389ab4
Paweł Stech <pawelstech95@gmail.com>
2021-03-01 14:32:43 +0100

    Added logic

--- Start/Item.js
+++ Start/Item.js
@@ -1,5 +1,9 @@
 const Item = (props) => (
-  <li style={props.active ? { fontWeight: 'bold' } : { color: 'gray' }}>
+  <li
+    className={props.active ? 'enabled' : 'disabled'}
+    style={props.active ? { fontWeight: 'bold' } : { color: 'gray' }}
+    onClick={() => props.changeStatus(props.id)}
+  >
     {props.name}
   </li>
 );
--- Start/app.js
+++ Start/app.js
@@ -657,12 +657,29 @@ class App extends React.Component {
       { id: 6, name: 'chleb', active: false },
     ],
   };
+  handleChangeStatus = (id) => {
+    // console.log(id);
+
+    const items = this.state.items.map((item) => {
+      if (id === item.id) {
+        item.active = !item.active;
+      }
+      return item;
+    });
+    this.setState({
+      // items: items,
+      items
+    });
+  };
 
   render() {
     return (
       <>
         <Header items={this.state.items} />
-        <ListItems items={this.state.items} />
+        <ListItems
+          items={this.state.items}
+          changeStatus={this.handleChangeStatus}
+        />
       </>
     );
   }

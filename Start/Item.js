128656657bb716b7b3535afcff6414d13ed09b7c
Paweł Stech <pawelstech95@gmail.com>
2021-03-01 20:24:48 +0100

    controlled and uncontrolled component

--- Start/Item.js
+++ Start/Item.js
@@ -1,9 +1,9 @@
-const Item = (props) => (
-  <li
-    className={props.active ? 'enabled' : 'disabled'}
-    style={props.active ? { fontWeight: 'bold' } : { color: 'gray' }}
-    onClick={() => props.changeStatus(props.id)}
-  >
-    {props.name}
-  </li>
-);
+// const Item = (props) => (
+//   <li
+//     className={props.active ? 'enabled' : 'disabled'}
+//     // style={props.active ? { fontWeight: 'bold' } : { color: 'gray' }}
+//     onClick={() => props.changeStatus(props.id)}
+//   >
+//     {props.name}
+//   </li>
+// );

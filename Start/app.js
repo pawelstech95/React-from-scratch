719068ab347e1043b2be98c8f8750806fc99fe64
Paweł Stech <pawelstech95@gmail.com>
2021-03-01 22:03:00 +0100

    create one common function

--- Start/app.js
+++ Start/app.js
@@ -693,25 +693,36 @@ class Form extends React.Component {
     isLoved: true,
     number: '0',
   };
-  handleCityChange = (e) => {
-    this.setState({
-      city: e.target.value,
-    });
-  };
-  handleTextChange = (e) => {
-    this.setState({
-      text: e.target.value,
-    });
-  };
-  handleIsLovedChange = (e) => {
-    this.setState({
-      isLoved: e.target.checked,
-    });
-  };
-  handleVisitsNumberChange = (e) => {
-    this.setState({
-      number: e.target.value,
-    });
+  // handleCityChange = (e) => {
+  //   this.setState({
+  //     city: e.target.value,
+  //   });
+  // };
+  // handleTextChange = (e) => {
+  //   this.setState({
+  //     text: e.target.value,
+  //   });
+  // };
+  // handleIsLovedChange = (e) => {
+  //   this.setState({
+  //     isLoved: e.target.checked,
+  //   });
+  // };
+  // handleVisitsNumberChange = (e) => {
+  //   this.setState({
+  //     number: e.target.value,
+  //   });
+  // };
+  handleChange = (e) => {
+    console.log(e.target.type);
+    if (e.target.type === 'checkbox') {
+      this.setState({
+        [e.target.name]: e.target.checked,
+      });
+    } else
+      this.setState({
+        [e.target.name]: e.target.value,
+      });
   };
   render() {
     return (
@@ -719,8 +730,9 @@ class Form extends React.Component {
         <label>
           Podaj miasto{' '}
           <input
+            name="city"
             value={this.state.city}
-            onChange={this.handleCityChange}
+            onChange={this.handleChange}
             type="text"
           />
         </label>
@@ -728,8 +740,9 @@ class Form extends React.Component {
         <label>
           Napisz coś o mieście
           <textarea
+            name="text"
             value={this.state.text}
-            onChange={this.handleTextChange}
+            onChange={this.handleChange}
           ></textarea>
         </label>
         <br />
@@ -737,17 +750,19 @@ class Form extends React.Component {
           {' '}
           Czy lubisz to miasto?
           <input
+            name="isLoved"
             type="checkbox"
             checked={this.state.isLoved}
-            onChange={this.handleIsLovedChange}
+            onChange={this.handleChange}
           />
         </label>
         <br />
         <label>
           Ile razy byliście w tym mieście?
           <select
+            name="number"
             value={this.state.number}
-            onChange={this.handleVisitsNumberChange}
+            onChange={this.handleChange}
           >
             <option value="0">0</option>
             <option value="1">1</option>

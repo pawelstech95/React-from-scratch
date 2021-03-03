432e867b7d0c277b458da44a4ec74b6ae83ad065
Paweł Stech <pawelstech95@gmail.com>
2021-03-03 21:20:59 +0100

    Create numbers draw

--- Start/app.js
+++ Start/app.js
@@ -790,55 +790,55 @@
 //   return <div>Wartość w euro: {props.cash <= 0 ? '' : value}</div>;
 // };
 
-const Cash = (props) => {
-  const value = (props.cash / props.ratio).toFixed(2);
-  return <div>Wartość w euro: {props.cash <= 0 ? '' : value}</div>;
-};
+// const Cash = (props) => {
+//   const value = (props.cash / props.ratio).toFixed(2);
+//   return <div>Wartość w euro: {props.cash <= 0 ? '' : value}</div>;
+// };
 
-class ExchangeCounter extends React.Component {
-  state = {
-    amount: '',
-    // ratioDollar: 3.6,
-    // ratioEuro: 4.2,
-  };
-  currencies = [
-    { id: 1, name: 'dollar', ratio: 3.6, title: 'Wartość w dolarach: ' },
-    { id: 2, name: 'euro', ratio: 4.1, title: 'Wartość w euro: ' },
-    { id: 3, name: 'pound', ratio: 4.6, title: 'Wartość w funtach: ' },
-  ];
+// class ExchangeCounter extends React.Component {
+//   state = {
+//     amount: '',
+//     // ratioDollar: 3.6,
+//     // ratioEuro: 4.2,
+//   };
+//   currencies = [
+//     { id: 1, name: 'dollar', ratio: 3.6, title: 'Wartość w dolarach: ' },
+//     { id: 2, name: 'euro', ratio: 4.1, title: 'Wartość w euro: ' },
+//     { id: 3, name: 'pound', ratio: 4.6, title: 'Wartość w funtach: ' },
+//   ];
 
-  handleChange = (e) => {
-    this.setState({
-      amount: e.target.value,
-    });
-  };
-  render() {
-    const { amount } = this.state;
-    const calculators = this.currencies.map((currency) => (
-      <Cash
-        key={currency.id}
-        cash={amount}
-        ratio={currency.ratio}
-        title={currency.title}
-      />
-    ));
-    return (
-      <div className="app">
-        <label>
-          <input
-            type="number"
-            name=""
-            value={amount}
-            onChange={this.handleChange}
-          />
-        </label>
-        {calculators}
-        {/* <Dollars cash={amount} ratio={ratioDollar} />
-        <Euros cash={amount} ratio={ratioEuro} /> */}
-        {/* <Cash cash={amount} ratio={ratioDollar} title="Wartość w dolarach: " />
-        <Cash cash={amount} ratio={ratioEuro} title="Wartość w Euro: " /> */}
-      </div>
-    );
-  }
-}
-ReactDOM.render(<ExchangeCounter />, document.getElementById('root'));
+//   handleChange = (e) => {
+//     this.setState({
+//       amount: e.target.value,
+//     });
+//   };
+//   render() {
+//     const { amount } = this.state;
+//     const calculators = this.currencies.map((currency) => (
+//       <Cash
+//         key={currency.id}
+//         cash={amount}
+//         ratio={currency.ratio}
+//         title={currency.title}
+//       />
+//     ));
+//     return (
+//       <div className="app">
+//         <label>
+//           <input
+//             type="number"
+//             name=""
+//             value={amount}
+//             onChange={this.handleChange}
+//           />
+//         </label>
+//         {calculators}
+//         {/* <Dollars cash={amount} ratio={ratioDollar} />
+//         <Euros cash={amount} ratio={ratioEuro} /> */}
+//         {/* <Cash cash={amount} ratio={ratioDollar} title="Wartość w dolarach: " />
+//         <Cash cash={amount} ratio={ratioEuro} title="Wartość w Euro: " /> */}
+//       </div>
+//     );
+//   }
+// }
+// ReactDOM.render(<ExchangeCounter />, document.getElementById('root'));
---
+++ Task for self-completion - divination draw/app.js
@@ -0,0 +1,27 @@
+
+
+class Draw extends React.Component {
+  state = {
+    options: ['1', '2', '3'],
+    option: null,
+  };
+  handleShowOption = () => {
+    const index = Math.floor(Math.random() * this.state.options.length);
+    console.log(index);
+    this.setState({
+      option: this.state.options[index]
+    })
+  };
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleShowOption}>Zobacz wróżbę</button><br />
+        {this.state.option ? <h1>{this.state.option}</h1> : null}
+      </div>
+    );
+  }
+}
+
+
+ReactDOM.render(<Draw />, document.getElementById('root'));

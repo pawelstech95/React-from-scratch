f91282a8de2f4953d9f130e74b4cf8c746695619
Paweł Stech <pawelstech95@gmail.com>
2021-03-12 17:04:17 +0100

    geting data from a dynamic source

--- App/create/src/components/App.js
+++ App/create/src/components/App.js
@@ -1,34 +1,51 @@
 import React, { Component } from 'react';
 import './App.css';
-import SwitchButton from './SwitchButton';
+// pseudo rosnaca baza danych - symuluje zewnetrzne dane
+const data = [
+  { id: 1, title: 'Wiadomość nr 1', body: 'Zawartość wiadomości nr 1...' },
+  { id: 2, title: 'Wiadomość nr 2', body: 'Zawartość wiadomości nr 2...' },
+];
+
+setInterval(() => {
+  const index = data.length + 1;
+  data.push({
+    id: index,
+    title: `Wiadomość nr ${index}`,
+    body: `Zawartość wiadomości nr ${index}...`,
+  });
+  // console.log(data);
+}, 4000);
+
 class App extends Component {
   state = {
-    time: 0,
-    active: false,
+    // comments: data // tworzymy referencje a chcemy pobrać dane
+    comments: [...data], // pobieramy i tworzymy kopię tablicy - pobieramy teraz dane od razu - state nnie jest miejscem zeby pobierac dane poczatkowe
   };
-  handleClick = () => {
-    if (this.state.active) {
-      clearInterval(this.idInterval);
+  getData = () => {
+    if (this.state.comments.length !== data.length) {
+      console.log('aktualizacja');
+      this.setState({
+        comments: [...data], // znowu pobieramy tablice ale ni ebudujemy referencji...pobieramy dane i budujemmy nowa tablice
+      });
     } else {
-      this.idInterval = setInterval(() => this.addSecond(), 1000);
+      console.log('dane takie same, nie aktualizuje');
     }
-
-    this.setState({
-      active: !this.state.active,
-    });
   };
-  addSecond = () => {
-    this.setState({
-      time: this.state.time + 1,
-    });
-  };
+  componentDidMount() {
+    this.idI = setInterval(this.getData, 3000); // metode clearinterval musi odwolac sie do id
+  }
+  componentWillUnmount() {
+    clearInterval(this.idI);
+  }
   render() {
-    return (
-      <>
-        <p>{this.state.time}</p>
-        <SwitchButton click={this.handleClick} active={this.state.active}/>
-      </>
-    );
+    const comments = this.state.comments.map((comment) => (
+      <div key={comment.id}>
+        <h4>{comment.title}</h4>
+        <div>{comment.body}</div>
+      </div>
+    ));
+    console.log(comments);
+    return <div className="App">{comments.reverse()}</div>; // funkcja reverse zmienia kolejność wyświetlania
   }
 }
 export default App;
--- App/create/src/components/SwitchButton.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-const SwitchButton = (props) => (
-  <button onClick={props.click}>{props.active ? 'STOP' : 'START'}</button>
-);
-
-export default SwitchButton;
